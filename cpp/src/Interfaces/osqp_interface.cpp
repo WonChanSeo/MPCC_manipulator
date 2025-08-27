498cc77fa2b4ef7c70a0669908f0f9da2121f8e6
lnHerit <inherit0416@gmail.com>
2025-08-27 22:34:17 +0900

    [Feat] Env inference logging

--- cpp/src/Interfaces/osqp_interface.cpp
+++ cpp/src/Interfaces/osqp_interface.cpp
@@ -107,7 +107,7 @@ void OsqpInterface::setEnvData(const Eigen::MatrixX3d &obs_positions, const doub
 
         rb_[i].updateEnv(obs_positions, obs_radius, envcolNN_);
         auto time = std::chrono::high_resolution_clock::now();
-        // printf("Env data updated for step %zu at time: %lld ms\n", i, std::chrono::duration_cast<std::chrono::milliseconds>(time.time_since_epoch()).count());
+        printf("Env data updated for step %zu at time: %lld ms\n", i, std::chrono::duration_cast<std::chrono::milliseconds>(time.time_since_epoch()).count());
     }
 }
 
