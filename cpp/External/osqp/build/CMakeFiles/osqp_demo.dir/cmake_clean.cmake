bad8183bf2ab27e072519eabda033c0d18a34f8d
lnHerit <inherit0416@gmail.com>
2025-09-05 19:53:07 +0900

    [Modi] osqp added

---
+++ cpp/External/osqp/build/CMakeFiles/osqp_demo.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/osqp_demo.dir/examples/osqp_demo.c.o"
+  "CMakeFiles/osqp_demo.dir/examples/osqp_demo.c.o.d"
+  "CMakeFiles/osqp_demo.dir/examples/problems/cvxqp2_s.c.o"
+  "CMakeFiles/osqp_demo.dir/examples/problems/cvxqp2_s.c.o.d"
+  "CMakeFiles/osqp_demo.dir/examples/problems/hs21.c.o"
+  "CMakeFiles/osqp_demo.dir/examples/problems/hs21.c.o.d"
+  "CMakeFiles/osqp_demo.dir/examples/problems/hs35.c.o"
+  "CMakeFiles/osqp_demo.dir/examples/problems/hs35.c.o.d"
+  "CMakeFiles/osqp_demo.dir/examples/problems/largeqp.c.o"
+  "CMakeFiles/osqp_demo.dir/examples/problems/largeqp.c.o.d"
+  "CMakeFiles/osqp_demo.dir/examples/problems/primalc1.c.o"
+  "CMakeFiles/osqp_demo.dir/examples/problems/primalc1.c.o.d"
+  "CMakeFiles/osqp_demo.dir/examples/problems/qpcblend.c.o"
+  "CMakeFiles/osqp_demo.dir/examples/problems/qpcblend.c.o.d"
+  "CMakeFiles/osqp_demo.dir/examples/problems/qptest.c.o"
+  "CMakeFiles/osqp_demo.dir/examples/problems/qptest.c.o.d"
+  "out/osqp_demo"
+  "out/osqp_demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/osqp_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
