7762a55315f284de64858d27a655a95be0090884
lnHerit <inherit0416@gmail.com>
2025-09-24 19:46:07 +0900

    [Modi] Scaling on my own

--- cpp/External/osqp/lib/include/osqp/osqp_api_constants.h
+++ cpp/External/osqp/lib/include/osqp/osqp_api_constants.h
@@ -160,7 +160,7 @@ extern const char * OSQP_ERROR_MESSAGE[];
 # define OSQP_EPS_REL               (1E-3)
 # define OSQP_EPS_PRIM_INF          (1E-4)
 # define OSQP_EPS_DUAL_INF          (1E-4)
-# define OSQP_SCALED_TERMINATION    (0)
+# define OSQP_SCALED_TERMINATION    (0) 
 # define OSQP_TIME_LIMIT            (1e10)     ///< Disable time limit by default
 
 // Disable the duality gap termination criteria on float builds by default for now, because
