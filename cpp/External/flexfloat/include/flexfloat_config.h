e4d4addeb7b99276652eb869574456066bd5c46b
lnHerit <inherit0416@gmail.com>
2025-11-17 09:27:16 +0900

    [Feat] quantize ongoing

--- cpp/External/flexfloat/include/flexfloat_config.h
+++ cpp/External/flexfloat/include/flexfloat_config.h
@@ -1,8 +1,12 @@
 #ifndef FLEXFLOAT_CONFIG_H_INCLUDED
 #define FLEXFLOAT_CONFIG_H_INCLUDED
 
-/* Using double precision as backend (default) */
+/* Using double precision (FP64) as backend for OSQP stability */
 #define FLEXFLOAT_ON_DOUBLE
+
+/* Note: FP32 backend (FLEXFLOAT_ON_SINGLE) causes OSQP KKT matrix to be non-convex */
+/* Only use FP32 if OSQP FlexFloat is disabled (USE_FLEXFLOAT=OFF) */
+// #define FLEXFLOAT_ON_SINGLE
 
 /* __float128 support available */
 #define FLEXFLOAT_FLOAT128
